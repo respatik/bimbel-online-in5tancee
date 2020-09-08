5df51afe6047df066f9c2019ca5a98e821eba84a
krisna firdaus <krisnafirdaus@gmail.com>
2020-09-09 01:33:15 +0700

    login dan register

---
+++ assets/js/js-logreg/login.js
@@ -0,0 +1,22 @@
+function login() {
+  let emailInput = document.getElementById("email").value;
+  let passwordInput = document.getElementById("password").value;
+
+  fetch("https://5ee447e15dd8b80016082df8.mockapi.io/users")
+    .then((response) => response.json())
+    .then((result) => {
+      let user = result.filter((item) => item.email === emailInput);
+      console.log("hasil user setelah filter", user);
+      if (user.length > 0) {
+        // alert("username anda ditemukan");
+        if (user[0].password === passwordInput) {
+          // simpan data user yang login ke localstorage
+          localStorage.setItem("user", JSON.stringify(user[0]));
+        } else {
+          alert("password anda salah");
+        }
+      } else {
+        alert("username tidak ditemukan");
+      }
+    });
+}
---
+++ assets/js/js-logreg/registration.js
@@ -0,0 +1,30 @@
+// buat function async await register
+const register = async () => {
+  let nameInput = document.getElementById("name").value;
+  let birthdayInput = document.getElementById("birthday").value;
+  let passwordInput = document.getElementById("password").value;
+  let emailInput = document.getElementById("email").value;
+  let phoneInput = document.getElementById("phone").value;
+
+  let userData = {
+    nama: nameInput,
+    birthDate: birthdayInput,
+    password: passwordInput,
+    email: emailInput,
+    phone: phoneInput,
+  };
+  // post data user
+
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+  let response = await fetch(
+    "https://5ee447e15dd8b80016082df8.mockapi.io/users/",
+    options
+  );
+  let result = await response.json();
+};
