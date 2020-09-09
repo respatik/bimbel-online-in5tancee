87ca6a2f3758d1825caf39308f2ff963b21527dd
krisna firdaus <krisnafirdaus@gmail.com>
2020-09-09 13:10:09 +0700

    krisna

--- assets/js/checkOutPembayaran.js
+++ assets/js/checkOutPembayaran.js
@@ -1,34 +1,34 @@
-const showPayment = async() =>{
-    try {
-        let dataPaket = JSON.parse(localStorage.getItem("dataPaket"));
-        // console.log(dataPaket.id);
-        // console.log(dataPaket.packageName);
-        // console.log(dataPaket.price)
-        document.getElementById('productName').innerHTML = dataPaket.packageName;
-        document.getElementById('productPrice').innerHTML = dataPaket.price;
-        document.getElementById('totalPrice').innerHTML = dataPaket.price;
-        document.getElementById('proceed').addEventListener('click', function(){
-            swal("Transaksi Berhasil" ,"" ,"success");
-        })
-    } catch (error) {
-        alert("maaf terjadi error pada data yang anda inginkan")
-    }
-}
+const showPayment = async () => {
+  try {
+    let dataPaket = JSON.parse(localStorage.getItem("dataPaket"));
+    // console.log(dataPaket.id);
+    // console.log(dataPaket.packageName);
+    // console.log(dataPaket.price)
+    document.getElementById("productName").innerHTML = dataPaket.packageName;
+    document.getElementById("productPrice").innerHTML = dataPaket.price;
+    document.getElementById("totalPrice").innerHTML = dataPaket.price;
+    document.getElementById("proceed").addEventListener("click", function () {
+      window.location.replace("/success/success.html");
+    });
+  } catch (error) {
+    alert("maaf terjadi error pada data yang anda inginkan");
+  }
+};
 
 showPayment();
 
-const popupDataUser = async() =>{
-    try {
-        let dataUser = JSON.parse(localStorage.getItem('user'));
-        console.log(dataUser.email)
-        console.log(dataUser)
-        let button = document.getElementById('icon-user');
-        button.setAttribute('title', dataUser.nama)
-        button.setAttribute('data-content', dataUser.email)
-        console.log(button);
-    } catch (error) {
-        alert('maaf terjadi kesalahan pada data')
-    }
-}
+const popupDataUser = async () => {
+  try {
+    let dataUser = JSON.parse(localStorage.getItem("user"));
+    console.log(dataUser.email);
+    console.log(dataUser);
+    let button = document.getElementById("icon-user");
+    button.setAttribute("title", dataUser.nama);
+    button.setAttribute("data-content", dataUser.email);
+    console.log(button);
+  } catch (error) {
+    alert("maaf terjadi kesalahan pada data");
+  }
+};
 
 popupDataUser();
