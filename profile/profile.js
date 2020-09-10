d42f412e1087a0287ac0f68251a33cf6e34bc6ff
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-10 11:11:22 +0700

    adding data profile

--- profile/profile.js
+++ profile/profile.js
@@ -6,3 +6,20 @@ function logout(){
   localStorage.clear();
   window.location.replace('../home.html')
 }
+
+const showDataUser = async () =>{
+  try {
+    let dataProfile = JSON.parse(localStorage.getItem('user'));
+    console.log(dataProfile)
+    document.getElementById('user-name').innerHTML = dataProfile.nama;
+    document.getElementById('user-id').innerHTML = dataProfile.id;
+    document.getElementById('profile-name').innerHTML = dataProfile.nama;
+    document.getElementById('user-email').innerHTML = dataProfile.email;
+    document.getElementById('user-phone').innerHTML = dataProfile.phone;
+    document.getElementById('user-birthdate').innerHTML = dataProfile.birthDate;
+  } catch (error) {
+    alert("maaf terjadi kesalahan data profile")
+  }
+}
+
+showDataUser()
