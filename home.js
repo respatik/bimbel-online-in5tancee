0a9f1cb2611d5ac6288fb7f1a71e8989a87d5956
respatik <respatik@gmail.com>
2020-09-10 22:25:13 +0700

    fixed the api for why

--- home.js
+++ home.js
@@ -58,25 +58,25 @@ fetch('https://5f51a92b5e98480016123c15.mockapi.io/why')
         console.log("waduh error");
     })
 
-let why = [
-    {
-        nama: "Tersedia Konselor Profesional",
-        detail: "Kamu bingung sama pilihan jurusanmu? Atau kamu ingin berkeluh kesah mengenai drama-dramamu selama kelas 3 SMA? Tenang! Di !n5tancee terdapat Konselor Profesional yang dapat memberikan arahan-arahan untuk masa depanmu yang lebih cerah :)",
-        img: "assets/images/consulting_monochromatic.png"
-    },
-    {
-        nama: "Tersedia Mentor-Mentor Handal yang Siap Menjawab Segala Pertanyaanmu",
-        detail: "Bingung kenapa jawabanmu masih salah padahal sudah sering bertemu soal yang sama? Di !n5tancee terdapat mentor-mentor handal yang selalu siap menjawab pertanyaanmu.",
-        img: "assets/images/online-learning.png"
-    },
-    {
-        nama: "Tersedia Progres di Setiap Try Out untuk Memantau Perkembangan Belajarmu",
-        detail: "Kamu ingin tahu perkembangan nilai Try Out? !n5tancee akan memberikanmu grafik perkembangan setiap kamu selesai Try Out. Dengan grafik yang kami berikan, kamu bisa tahu sudah sampai mana kemampuan mengerjakan soalmu",
-        img: "assets/images/progress__monochromatic.png"
-    },
-    {
-        nama: "Tersedia Video Tutorial dari Semua pelajaran",
-        detail: "Kamu tipe belajar visual yang lebih cepat memahami pelajaran dengan video? !n5tancee menyediakan Video Tutorial dari mentor-mentor dengan gaya mengajar dan bahasa yang asik! Dijamin kamu tidak akan bosan deh!",
-        img: "assets/images/video_tutorial__monochromatic.png"
-    }
-]
+// let why = [
+//     {
+//         nama: "Tersedia Konselor Profesional",
+//         detail: "Kamu bingung sama pilihan jurusanmu? Atau kamu ingin berkeluh kesah mengenai drama-dramamu selama kelas 3 SMA? Tenang! Di !n5tancee terdapat Konselor Profesional yang dapat memberikan arahan-arahan untuk masa depanmu yang lebih cerah :)",
+//         img: "assets/images/consulting_monochromatic.png"
+//     },
+//     {
+//         nama: "Tersedia Mentor-Mentor Handal yang Siap Menjawab Segala Pertanyaanmu",
+//         detail: "Bingung kenapa jawabanmu masih salah padahal sudah sering bertemu soal yang sama? Di !n5tancee terdapat mentor-mentor handal yang selalu siap menjawab pertanyaanmu.",
+//         img: "assets/images/online-learning.png"
+//     },
+//     {
+//         nama: "Tersedia Progres di Setiap Try Out untuk Memantau Perkembangan Belajarmu",
+//         detail: "Kamu ingin tahu perkembangan nilai Try Out? !n5tancee akan memberikanmu grafik perkembangan setiap kamu selesai Try Out. Dengan grafik yang kami berikan, kamu bisa tahu sudah sampai mana kemampuan mengerjakan soalmu",
+//         img: "assets/images/progress__monochromatic.png"
+//     },
+//     {
+//         nama: "Tersedia Video Tutorial dari Semua pelajaran",
+//         detail: "Kamu tipe belajar visual yang lebih cepat memahami pelajaran dengan video? !n5tancee menyediakan Video Tutorial dari mentor-mentor dengan gaya mengajar dan bahasa yang asik! Dijamin kamu tidak akan bosan deh!",
+//         img: "assets/images/video_tutorial__monochromatic.png"
+//     }
+// ]
